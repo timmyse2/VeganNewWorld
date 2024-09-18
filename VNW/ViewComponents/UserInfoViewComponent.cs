721da4596b56b5e88083bda02b555cf5918ea8a6
timmy <timmyse2@yahoo.com.tw>
2024-09-18 16:53:52 +0800

    View Component - set 'User info' icon in layout nav <li>; 2B only;

---
+++ VNW/ViewComponents/UserInfoViewComponent.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+using VNW.Models; //for _context
+using System.Threading.Tasks; //for task
+
+namespace VNW.ViewComponents
+{
+    public class UserInfoViewComponent : ViewComponent
+    {
+        //private readonly DatabaseContext _context;
+        private readonly VeganNewWorldContext _context;
+        VNW.Common.MySession _ms = new Common.MySession();
+        public UserInfoViewComponent(VeganNewWorldContext context)
+        {
+            _context = context;
+        }
+
+        //public async Task<IViewComponentResult> InvokeAsync(int TopPricing)
+        //{
+
+        //    //var products = await _context.Products
+        //    //                             .OrderByDescending(p => p.Price)
+        //    //                             .Take(TopPricing)
+        //    //                             .ToListAsync();
+
+        //    //return View(products);
+        //    //return View("MyProduct", products);
+        //    return View();
+        //}
+
+        //public IViewComponentResult Invoke()
+        //{
+        //    return View();
+        //}
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.ShopAccount = _ms.GetMySession("ShopAccount", HttpContext.Session);
+            ViewData["UserIcon"] = _ms.GetMySession("UserIcon", HttpContext.Session);
+            return View();
+        }
+    }    
+}
+
+
