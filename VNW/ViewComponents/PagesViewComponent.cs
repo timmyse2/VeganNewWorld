12bf228f0869ddae269c026dd0b62dde3a1d6465
timmy <timmyse2@yahoo.com.tw>
2024-09-23 10:29:51 +0800

    ProductList - use VC pagination, add "PagesViewComponent" <vc:pages>  NOT READY

---
+++ VNW/ViewComponents/PagesViewComponent.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+using VNW.Models; //for _context
+using System.Threading.Tasks; //for task
+
+namespace VNW.ViewComponents
+{
+    public class PagesViewComponent : ViewComponent
+    {
+        //private readonly DatabaseContext _context;
+        private readonly VeganNewWorldContext _context;
+        VNW.Common.MySession _ms = new Common.MySession();
+        public PagesViewComponent(VeganNewWorldContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }    
+}
+
+
